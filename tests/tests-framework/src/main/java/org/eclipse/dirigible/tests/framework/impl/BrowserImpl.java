10560045fb52257e9005b34ce293b3da152903f8
Valentin <79216061+alt-plus-f4@users.noreply.github.com>
2025-02-05 15:31:34 +0200

    Changed DirigibleHomepageIT test to work with new UI (#4617)
    ---------
    
    Signed-off-by: Iliyan Velichkov <velichkov.iliyan@gmail.com>
    Co-authored-by: Iliyan Velichkov <velichkov.iliyan@gmail.com>

--- tests/tests-framework/src/main/java/org/eclipse/dirigible/tests/framework/impl/BrowserImpl.java
+++ tests/tests-framework/src/main/java/org/eclipse/dirigible/tests/framework/impl/BrowserImpl.java
@@ -27,7 +27,6 @@ import org.springframework.stereotype.Component;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
-import java.util.regex.Pattern;
 
 import static org.assertj.core.api.Assertions.fail;
 
@@ -271,15 +270,13 @@ class BrowserImpl implements Browser {
 
     @Override
     public void doubleClickOnElementContainingText(HtmlElementType elementType, String text) {
-        String textPattern = Pattern.quote(text);
-        SelenideElement element = getElementByAttributeAndTextPattern(elementType, textPattern);
+        SelenideElement element = getElementByAttributeAndTextPattern(elementType, text);
         handleElementInAllFrames(element, SelenideElement::doubleClick);
     }
 
     @Override
     public void clickOnElementContainingText(HtmlElementType elementType, String text) {
-        String textPattern = Pattern.quote(text);
-        SelenideElement element = getElementByAttributeAndTextPattern(elementType, textPattern);
+        SelenideElement element = getElementByAttributeAndTextPattern(elementType, text);
         handleElementInAllFrames(element, SelenideElement::click);
     }
 
@@ -296,6 +293,10 @@ class BrowserImpl implements Browser {
     }
 
     private SelenideElement getElementByAttributeAndTextPattern(HtmlElementType htmlElementType, String textPattern) {
+        if (htmlElementType == HtmlElementType.SPAN) {
+            String escapedTextPattern = textPattern.replace("\"", "\\\"");
+            return Selenide.$x("//span[contains(text(), \"" + escapedTextPattern + "\")]");
+        }
         ElementsCollection elements = getElements(htmlElementType);
         return elements.findBy(Condition.matchText(textPattern));
     }
@@ -307,6 +308,11 @@ class BrowserImpl implements Browser {
     }
 
     private SelenideElement getElementByAttributeAndText(HtmlElementType elementType, String text) {
+        if (elementType == HtmlElementType.SPAN) {
+            String escapedText = text.replace("\"", "\\\"");
+            return Selenide.$x("//span[text()=\"" + escapedText + "\"]");
+        }
+
         By selector = constructCssSelectorByType(elementType);
         ElementsCollection options = Selenide.$$(selector);
 
