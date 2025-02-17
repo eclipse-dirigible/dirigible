0afbf25a3e8b06360b42aeb3831efedb6c68c2f0
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-02-17 13:22:52 +0200

    switch tests to the new UI
    Signed-off-by: Iliyan Velichkov <velichkov.iliyan@gmail.com>

--- tests/tests-integrations/src/test/java/org/eclipse/dirigible/integration/tests/ui/tests/MultitenancyIT.java
+++ tests/tests-integrations/src/test/java/org/eclipse/dirigible/integration/tests/ui/tests/MultitenancyIT.java
@@ -27,10 +27,6 @@ import java.util.List;
 
 class MultitenancyIT extends UserInterfaceIntegrationTest {
 
-    static {
-        Configuration.set("DIRIGIBLE_HOME_URL", "services/web/ide/");
-    }
-
     @Autowired
     private TestProject testProject;
     @Autowired
