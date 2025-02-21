65c92589f88410b1200659461012d0dc5cdbe22d
ThuF <jordan.p.pavlov@gmail.com>
2025-02-21 16:19:45 +0200

    [Security] AWS Cognito - Add Support for Roles/Groups

--- components/security/security-oauth2/src/main/java/org/eclipse/dirigible/components/security/oauth2/OAuth2SecurityConfiguration.java
+++ components/security/security-oauth2/src/main/java/org/eclipse/dirigible/components/security/oauth2/OAuth2SecurityConfiguration.java
@@ -9,6 +9,9 @@
  */
 package org.eclipse.dirigible.components.security.oauth2;
 
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.stream.Collectors;
 import org.eclipse.dirigible.components.base.http.access.HttpSecurityURIConfigurator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +19,9 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -40,12 +46,25 @@ public class OAuth2SecurityConfiguration {
             .csrf(csrf -> csrf.disable())
             .headers(headers -> headers.frameOptions(frameOpts -> frameOpts.disable()))
             .oauth2Client(Customizer.withDefaults())
-            .oauth2Login(Customizer.withDefaults())
+            .oauth2Login(oauth2 -> {
+                oauth2.userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig.userAuthoritiesMapper(userAuthoritiesMapper()));
+            })
             .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
             .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.ALWAYS));
 
         httpSecurityURIConfigurator.configure(http);
 
         return http.build();
+    }
+
+    @Bean
+    public GrantedAuthoritiesMapper userAuthoritiesMapper() {
+        return (authorities) -> {
+            OidcUserAuthority oidcUserAuthority = (OidcUserAuthority) new ArrayList<>(authorities).get(0);
+            Map<String, Object> attributes = oidcUserAuthority.getAttributes();
+            return ((ArrayList<?>) attributes.get("cognito:groups")).stream()
+                                                                    .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                                                                    .collect(Collectors.toSet());
+        };
     }
 }
