c8e36ae2d23af3433643f7dd643d1360c7126e64
bob4o-afk <bobkatasad@abv.bg>
2025-02-25 18:13:37 +0200

    feat: adding tests for hyperion

---
+++ tests/tests-integrations/src/test/resources/leave-request/gen/submit-leave-request/forms/submit-leave-request/controller.js
@@ -0,0 +1,26 @@
+const formView = angular.module('forms', ['ideUI', 'ideView']);
+
+formView.controller('FormController', ['$scope', '$http', function ($scope, $http) {
+
+    $scope.forms = {
+        form: {}
+    };
+
+    $scope.model = {};
+
+    $scope.model.fromDate = new Date();
+    $scope.model.toDate = new Date();
+    
+    $scope.onSubmitClicked = function(){
+        console.log("Model:" + JSON.stringify($scope.model));
+        $http.post("/services/ts/leave-request/api/ProcessService.ts/requests", JSON.stringify($scope.model)).then(function (response) {
+            if (response.status != 202) {
+                alert(`Unable to create new leave request: '${response.message}'`);
+                return;
+            }
+            alert("Leave request has been created.\nResponse: " + JSON.stringify(response.data));
+        });
+    }
+    
+
+}]);
