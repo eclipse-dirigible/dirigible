d4ae345ab2f8e7f4a835385af3fdec505fa14f95
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-03-04 12:11:05 +0200

    register all new file options to complete the test
    Signed-off-by: Iliyan Velichkov <velichkov.iliyan@gmail.com>

--- tests/tests-integrations/src/test/java/org/eclipse/dirigible/integration/tests/ui/tests/NewFileOption.java
+++ tests/tests-integrations/src/test/java/org/eclipse/dirigible/integration/tests/ui/tests/NewFileOption.java
@@ -11,27 +11,24 @@
 package org.eclipse.dirigible.integration.tests.ui.tests;
 
 enum NewFileOption {
-    ENTITY_DATA_MODEL("Entity Data Model", "edm.edm");
-
-    // TODO to be added new file name
-    // FILE("File"), //
-    // FOLDER("Folder"), //
-    // JAVASCRIPT_SERVICE("JavaScript Service"), //
-    // TYPESCRIPT_SERVICE("TypeScript Service"), //
-    // BUSINESS_PROCESS_MODEL("Business Process Model"), //
-    // ACCESS_CONSTRAINTS("Access Constraints"), //
-    // DATABASE_SCHEMA_MODEL("Database Schema Model"), //
-    // DATABASE_TABLE("Database Table"), //
-    // DATABASE_VIEW("Database View"), //
-    // EXTENSION("Extension"), //
-    // EXTENSION_POINT("Extension Point"), //
-    // FORM_DEFINITION("Form Definition"), //
-    // HTML5_PAGE("HTML5 Page"), //
-    // MESSAGE_LISTENER("Message Listener"), //
-    // REPORT_MODEL("Report Model"), //
-    // ROLES_DEFINITION("Roles Definition"), //
-    // SCHEDULED_JOB("Scheduled Job"), //
-    // WEBSOCKET("WebSocket");
+    ENTITY_DATA_MODEL("Entity Data Model", "edm.edm"), //
+    FILE("File", "aaa"), //
+    JAVASCRIPT_SERVICE("JavaScript Service", "javascript-esm.mjs"), //
+    TYPESCRIPT_SERVICE("TypeScript Service", "typescript.ts"), //
+    BUSINESS_PROCESS_MODEL("Business Process Model", "bpmn.bpmn"), //
+    ACCESS_CONSTRAINTS("Access Constraints", "access.access"), //
+    DATABASE_SCHEMA_MODEL("Database Schema Model", "schema.dsm"), //
+    DATABASE_TABLE("Database Table", "database-table.table"), //
+    DATABASE_VIEW("Database View", "database-view.view"), //
+    EXTENSION("Extension", "extension.extension"), //
+    EXTENSION_POINT("Extension Point", "extensionpoint.extensionpoint"), //
+    FORM_DEFINITION("Form Definition", "form.form"), //
+    MESSAGE_LISTENER("Message Listener", "listener.listener"), //
+    REPORT_MODEL("Report Model", "report-model.report"), //
+    ROLES_DEFINITION("Roles Definition", "roles.roles"), //
+    SCHEDULED_JOB("Scheduled Job", "job.job"), //
+    WEBSOCKET("WebSocket", "websocket.websocket"), //
+    HTML5_PAGE("HTML5 Page", "html.html"); //
 
     private final String optionName;
 
