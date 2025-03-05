23a17512fd9cb9e0a49ee55a9fb268eb352d5ef9
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-03-05 14:34:23 +0200

    more adaptations
    Signed-off-by: Iliyan Velichkov <velichkov.iliyan@gmail.com>

--- tests/tests-integrations/src/test/java/org/eclipse/dirigible/integration/tests/services/integrations/CamelDirigibleJavaScriptComponentHttpRouteIT.java
+++ tests/tests-integrations/src/test/java/org/eclipse/dirigible/integration/tests/services/integrations/CamelDirigibleJavaScriptComponentHttpRouteIT.java
@@ -9,57 +9,17 @@
  */
 package org.eclipse.dirigible.integration.tests.services.integrations;
 
-import ch.qos.logback.classic.Level;
-import org.eclipse.dirigible.integration.tests.ui.tests.UserInterfaceIntegrationTest;
-import org.eclipse.dirigible.tests.logging.LogsAsserter;
-import org.eclipse.dirigible.tests.restassured.RestAssuredExecutor;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.eclipse.dirigible.integration.tests.ui.tests.projects.PredefinedProjectIT;
+import org.eclipse.dirigible.integration.tests.ui.tests.projects.TestProject;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.concurrent.TimeUnit;
-
-import static io.restassured.RestAssured.given;
-import static org.awaitility.Awaitility.await;
-import static org.hamcrest.Matchers.containsString;
-
-class CamelDirigibleJavaScriptComponentHttpRouteIT extends UserInterfaceIntegrationTest {
+class CamelDirigibleJavaScriptComponentHttpRouteIT extends PredefinedProjectIT {
 
     @Autowired
-    private RestAssuredExecutor restAssuredExecutor;
-    private LogsAsserter logsAsserter;
+    private CamelDirigibleJavaScriptComponentHttpRouteTestProject testProject;
 
-    @BeforeEach
-    void setUp() {
-        this.logsAsserter = new LogsAsserter("CustomComponentLogger", Level.INFO);
+    @Override
+    protected TestProject getTestProject() {
+        return testProject;
     }
-
-    @Test
-    void testInvokeJSWithCronRoute() {
-        ide.createAndPublishProjectFromResources("CamelDirigibleJavaScriptComponentIT/cron-route/call-dirigible-js-custom-component");
-
-        assertBodyIsPassedAndHandledProperlyByJSHandler();
-    }
-
-    private void assertBodyIsPassedAndHandledProperlyByJSHandler() {
-        // this log message is expected to be logged by the final camel log step
-        await().atMost(20, TimeUnit.SECONDS)
-               .until(() -> logsAsserter.containsMessage("Completed execution. Body: [MY TEST BODY]", Level.INFO));
-    }
-
-    @Test
-    void testInvokeJSWithEndpointRoute() {
-        ide.createAndPublishProjectFromResources(
-                "CamelDirigibleJavaScriptComponentIT/http-route/http-route-call-dirigible-js-custom-component");
-
-        restAssuredExecutor.execute( //
-                () -> given().when()
-                             .get("/services/integrations/http-route")
-                             .then()
-                             .statusCode(200)
-                             .body(containsString("Body set by the handler")),
-                25);
-
-    }
-
 }
