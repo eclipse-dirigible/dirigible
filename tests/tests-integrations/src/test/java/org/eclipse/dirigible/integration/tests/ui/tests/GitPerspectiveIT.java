e0646db9c49a0b37cc0b89f13f4f1f0eae14879d
alt-plus-f4 <valentin.assenov@abv.bg>
2025-03-18 09:18:28 +0200

    feat: publish project

--- tests/tests-integrations/src/test/java/org/eclipse/dirigible/integration/tests/ui/tests/GitPerspectiveIT.java
+++ tests/tests-integrations/src/test/java/org/eclipse/dirigible/integration/tests/ui/tests/GitPerspectiveIT.java
@@ -2,11 +2,13 @@ package org.eclipse.dirigible.integration.tests.ui.tests;
 
 import org.eclipse.dirigible.tests.GitPerspective;
 import org.eclipse.dirigible.tests.UserInterfaceIntegrationTest;
+import org.eclipse.dirigible.tests.Workbench;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class GitPerspectiveIT extends UserInterfaceIntegrationTest {
     private GitPerspective gitPerspective;
+    private Workbench workbench;
 
     @BeforeEach
     void setUp() {
@@ -18,5 +20,8 @@ public class GitPerspectiveIT extends UserInterfaceIntegrationTest {
         this.gitPerspective = ide.openGitPerspective();
 
         gitPerspective.cloneRepository("https://github.com/codbex/codbex-sample-model-depends-on");
+
+        this.workbench = ide.openWorkbench();
+        workbench.publishAll(true);
     }
 }
