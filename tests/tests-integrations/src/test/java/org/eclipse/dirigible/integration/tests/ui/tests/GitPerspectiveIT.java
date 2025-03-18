6cab42d02b46d12bbbf4340722a40273abefaf85
alt-plus-f4 <valentin.assenov@abv.bg>
2025-03-18 09:11:37 +0200

    feat: add cloning repository functionality

--- tests/tests-integrations/src/test/java/org/eclipse/dirigible/integration/tests/ui/tests/GitPerspectiveIT.java
+++ tests/tests-integrations/src/test/java/org/eclipse/dirigible/integration/tests/ui/tests/GitPerspectiveIT.java
@@ -2,13 +2,24 @@ package org.eclipse.dirigible.integration.tests.ui.tests;
 
 import org.eclipse.dirigible.tests.GitPerspective;
 import org.eclipse.dirigible.tests.UserInterfaceIntegrationTest;
+import org.eclipse.dirigible.tests.util.SleepUtil;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class GitPerspectiveIT extends UserInterfaceIntegrationTest {
     private GitPerspective gitPerspective;
 
+    @BeforeEach
+    void setUp() {
+        browser.clearCookies();
+    }
+
     @Test
     void testGitFunctionality() {
         this.gitPerspective = ide.openGitPerspective();
+
+        gitPerspective.cloneRepository("https://github.com/codbex/codbex-sample-model-depends-on");
+
+        SleepUtil.sleepSeconds(1000);
     }
 }
