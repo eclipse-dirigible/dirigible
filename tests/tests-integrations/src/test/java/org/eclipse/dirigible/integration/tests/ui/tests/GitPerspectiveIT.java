018fdb8708e44f7067bde19daa1e255af98d43da
alt-plus-f4 <valentin.assenov@abv.bg>
2025-03-19 15:50:29 +0200

    feat: add sample project and tests

--- tests/tests-integrations/src/test/java/org/eclipse/dirigible/integration/tests/ui/tests/GitPerspectiveIT.java
+++ tests/tests-integrations/src/test/java/org/eclipse/dirigible/integration/tests/ui/tests/GitPerspectiveIT.java
@@ -1,17 +1,25 @@
 package org.eclipse.dirigible.integration.tests.ui.tests;
 
+import ch.qos.logback.classic.Level;
 import org.eclipse.dirigible.tests.GitPerspective;
 import org.eclipse.dirigible.tests.UserInterfaceIntegrationTest;
 import org.eclipse.dirigible.tests.Workbench;
+import org.eclipse.dirigible.tests.logging.LogsAsserter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.TimeUnit;
+
+import static org.awaitility.Awaitility.await;
+
 public class GitPerspectiveIT extends UserInterfaceIntegrationTest {
+    private LogsAsserter consoleLogAsserter;
     private GitPerspective gitPerspective;
     private Workbench workbench;
 
     @BeforeEach
     void setUp() {
+        this.consoleLogAsserter = new LogsAsserter("app.out", Level.INFO);
         browser.clearCookies();
     }
 
@@ -22,12 +30,14 @@ public class GitPerspectiveIT extends UserInterfaceIntegrationTest {
         gitPerspective.cloneRepository("https://github.com/codbex/codbex-sample-test-git");
 
         this.workbench = ide.openWorkbench();
-        workbench.clickPublishAll();
+        workbench.publishAll(true);
 
         assertWorkingProject();
     }
 
     void assertWorkingProject() {
-        // consoleLogAssrter
+        await().atMost(5, TimeUnit.SECONDS)
+               .pollInterval(1, TimeUnit.SECONDS)
+               .until(() -> consoleLogAsserter.containsMessage("GIT-PERSPECTIVE-VALIDATION-OK", Level.INFO));
     }
 }
