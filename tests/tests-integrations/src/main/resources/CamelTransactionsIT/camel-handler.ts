aa3fa9d2f95e9a6af10b625411e9c7f313885630
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-03-27 12:04:22 +0200

    fix camel transactions + add integration test

---
+++ tests/tests-integrations/src/main/resources/CamelTransactionsIT/camel-handler.ts
@@ -0,0 +1,13 @@
+import { BookRepository } from "./gen/edm/dao/Books/BookRepository";
+
+export function onMessage(message: any) {
+    const repo = new BookRepository();
+    const entity = {
+        Title: "test-camel-transactions-title-01",
+        Author: "test-camel-transactions-author-01"
+    }
+    repo.create(entity);
+
+    throw new Error("Intentionally throw error to check the Camel transactions logic");
+}
+
