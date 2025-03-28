bc860c492c8fde9f5a9269cdaa549e6d23e15baf
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-03-28 12:51:59 +0200

    use transaction managers in executor

--- components/data/data-sources/src/main/java/org/eclipse/dirigible/components/data/sources/config/TransactionExecutionException.java
+++ components/data/data-sources/src/main/java/org/eclipse/dirigible/components/data/sources/config/TransactionExecutionException.java
@@ -5,4 +5,8 @@ public class TransactionExecutionException extends RuntimeException {
     public TransactionExecutionException(String message, Throwable cause) {
         super(message, cause);
     }
+
+    public TransactionExecutionException(String message) {
+        super(message);
+    }
 }
