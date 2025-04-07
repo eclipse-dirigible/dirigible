9d320554efd3c865ca37c626eda4d7a69cb5be0f
alt-plus-f4 <valentin.assenov@abv.bg>
2025-04-07 12:41:12 +0300

    feat: add all tests

--- tests/tests-integrations/src/main/java/org/eclipse/dirigible/integration/tests/ui/tests/DependsOnScenariosTestProject.java
+++ tests/tests-integrations/src/main/java/org/eclipse/dirigible/integration/tests/ui/tests/DependsOnScenariosTestProject.java
@@ -26,8 +26,8 @@ import java.util.Map;
 class DependsOnScenariosTestProject extends BaseTestProject {
 
     private static final String PROJECT_RESOURCES_PATH = "DependsOnScenariosTestProject";
-    private static final String VERIFICATION_URI = "/services/web/" + PROJECT_RESOURCES_PATH + "/gen/sales-order/ui/Customer/index.html";
-
+    private static final String VERIFICATION_URI_CY = "/services/web/" + PROJECT_RESOURCES_PATH + "/gen/sales-order/ui/Customer/index.html";
+    private static final String VERIFICATION_URI_PM = "/services/web/" + PROJECT_RESOURCES_PATH + "gen/sales-order/ui/SalesOrder/index.html";
     private final Browser browser;
 
     DependsOnScenariosTestProject(IDE ide, ProjectUtil projectUtil, EdmView edmView, Browser browser) {
@@ -43,7 +43,7 @@ class DependsOnScenariosTestProject extends BaseTestProject {
 
     @Override
     public void verify() {
-        browser.openPath(VERIFICATION_URI);
+        browser.openPath(VERIFICATION_URI_CY);
 
         browser.clickElementByAttributes(HtmlElementType.BUTTON,
                 Map.of(HtmlAttribute.GLYPH, "sap-icon--add", HtmlAttribute.CLASS, "fd-button fd-button--transparent fd-button--compact"));
@@ -52,6 +52,76 @@ class DependsOnScenariosTestProject extends BaseTestProject {
 
         asserCountryCity("Bulgaria", "Sofia");
         asserCountryCity("Italy", "Rome");
+
+        // 2
+        browser.openPath(VERIFICATION_URI_PM);
+        browser.clickOnElementByAttributePatternAndText(HtmlElementType.DIV, HtmlAttribute.CLASS, "fd-list__title", "CustomerA");
+        browser.clickOnElementById("SalesOrderItem");
+        browser.clickElementByAttributes(HtmlElementType.BUTTON,
+                Map.of(HtmlAttribute.CLASS, "fd-button", HtmlAttribute.NGCLICK, "createEntity()"));
+        assertProductUom("ProductA", "Kg");
+        assertProductUom("ProductB", "Liter");
+
+        // 3
+        browser.openPath(VERIFICATION_URI_PM);
+        browser.clickOnElementByAttributePatternAndText(HtmlElementType.DIV, HtmlAttribute.CLASS, "fd-list__title", "CustomerA");
+        browser.clickOnElementById("SalesOrderItem");
+        browser.clickElementByAttributes(HtmlElementType.BUTTON,
+                Map.of(HtmlAttribute.CLASS, "fd-button", HtmlAttribute.NGCLICK, "createEntity()"));
+        assertProductPrice("ProductA", "11");
+        assertProductPrice("ProductB ", "20");
+
+        // 4
+        browser.openPath(VERIFICATION_URI_PM);
+        browser.clickOnElementByAttributePatternAndText(HtmlElementType.DIV, HtmlAttribute.CLASS, "fd-list__title", "CustomerA");
+        browser.clickOnElementById("SalesOrderPayment");
+        browser.clickElementByAttributes(HtmlElementType.BUTTON,
+                Map.of(HtmlAttribute.CLASS, "fd-button", HtmlAttribute.NGCLICK, "createEntity()"));
+        browser.clickOnElementByAttributePattern(HtmlElementType.INPUT, HtmlAttribute.PLACEHOLDER, "Search Customer ...");
+
+        // 5
+        browser.openPath(VERIFICATION_URI_PM);
+        browser.clickOnElementByAttributePatternAndText(HtmlElementType.DIV, HtmlAttribute.CLASS, "fd-list__title", "CustomerA");
+        browser.clickOnElementById("SalesOrderPayment");
+        browser.clickElementByAttributes(HtmlElementType.BUTTON,
+                Map.of(HtmlAttribute.CLASS, "fd-button", HtmlAttribute.NGCLICK, "createEntity()"));
+        assertCustomerPayment("CustomerA", "Payment 1");
+        assertCustomerPayment("CustomerB", "Payment 2");
+
+        // 6
+        browser.openPath(VERIFICATION_URI_PM);
+        browser.clickOnElementByAttributePatternAndText(HtmlElementType.DIV, HtmlAttribute.CLASS, "fd-list__title", "CustomerA");
+        browser.clickOnElementById("SalesOrderPayment");
+        browser.clickElementByAttributes(HtmlElementType.BUTTON,
+                Map.of(HtmlAttribute.CLASS, "fd-button", HtmlAttribute.NGCLICK, "createEntity()"));
+        assertCustomerPaymentAmount("CustomerA", "101");
+        assertCustomerPaymentAmount("CustomerB", "201");
+    }
+
+    private void assertCustomerPaymentAmount(String customer, String amount) {
+        browser.clickOnElementByAttributePattern(HtmlElementType.INPUT, HtmlAttribute.PLACEHOLDER, "Search Customer ...");
+        browser.clickOnElementByAttributePatternAndText(HtmlElementType.SPAN, HtmlAttribute.CLASS, "fd-list__title", customer);
+        browser.assertElementExistsByTypeAndContainsText(HtmlElementType.SPAN, amount);
+    }
+
+    private void assertCustomerPayment(String customer, String payment) {
+        browser.clickOnElementByAttributePattern(HtmlElementType.INPUT, HtmlAttribute.PLACEHOLDER, "Search Customer ...");
+        browser.clickOnElementByAttributePatternAndText(HtmlElementType.SPAN, HtmlAttribute.CLASS, "fd-list__title", customer);
+        browser.clickOnElementByAttributePattern(HtmlElementType.INPUT, HtmlAttribute.PLACEHOLDER, "Search CustomerPayment ...");
+        browser.assertElementExistsByTypeAndContainsText(HtmlElementType.SPAN, payment);
+    }
+
+    private void assertProductPrice(String product, String price) {
+        browser.clickOnElementByAttributePattern(HtmlElementType.INPUT, HtmlAttribute.PLACEHOLDER, "Search Product ...");
+        browser.clickOnElementByAttributePatternAndText(HtmlElementType.SPAN, HtmlAttribute.CLASS, "fd-list__title", product);
+        browser.assertElementExistsByTypeAndContainsText(HtmlElementType.SPAN, price);
+    }
+
+    private void assertProductUom(String product, String uom) {
+        browser.clickOnElementByAttributePattern(HtmlElementType.INPUT, HtmlAttribute.PLACEHOLDER, "Search Product ...");
+        browser.clickOnElementByAttributePatternAndText(HtmlElementType.SPAN, HtmlAttribute.CLASS, "fd-list__title", product);
+        browser.clickOnElementByAttributePattern(HtmlElementType.INPUT, HtmlAttribute.PLACEHOLDER, "Search UoM ...");
+        browser.assertElementExistsByTypeAndContainsText(HtmlElementType.SPAN, uom);
     }
 
     private void asserCountryCity(String country, String city) {
