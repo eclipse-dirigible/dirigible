95762c38c6a4ca5d3a899ccc335f0b68190f73fa
Nikol Georgieva <nicole.georgieva18@abv.bg>
2025-05-14 18:38:57 +0300

    add test for creating text file from the new menu

---
+++ tests/tests-integrations/src/main/java/org/eclipse/dirigible/integration/tests/ui/tests/CreateTextFileIT.java
@@ -0,0 +1,46 @@
+package org.eclipse.dirigible.integration.tests.ui.tests;
+
+import org.eclipse.dirigible.tests.base.UserInterfaceIntegrationTest;
+import org.eclipse.dirigible.tests.framework.browser.HtmlAttribute;
+import org.eclipse.dirigible.tests.framework.browser.HtmlElementType;
+import org.eclipse.dirigible.tests.framework.ide.Workbench;
+import org.junit.jupiter.api.Test;
+
+public class CreateTextFileIT extends UserInterfaceIntegrationTest {
+
+    public static final String FILE_NAME = "test.txt";
+    public static final String FILE_CONTENT = "my sample text";
+    public static final String PROJECT_NAME = "CreateTextFileIT";
+    public static final String PREVIEW_ID = "preview";
+
+    @Test
+    void test() {
+        Workbench workbench = ide.openWorkbench();
+        workbench.createNewProject(this.getClass()
+                .getSimpleName());
+
+        workbench.createCustomFileInProject(PROJECT_NAME,FILE_NAME );
+        workbench.openFile(FILE_NAME);
+
+        assertFileTabIsOpen(FILE_NAME);
+
+        workbench.addContentToFile(FILE_CONTENT);
+        workbench.saveAll();
+
+        assertTextIsPresent();
+    }
+
+    private void assertTextIsPresent() {
+        browser.clickOnElementById(PREVIEW_ID);
+        browser.assertElementExistsByTypeAndContainsText(HtmlElementType.PRE,FILE_CONTENT);
+    }
+
+    private void assertFileTabIsOpen(String fileName) {
+        browser.assertElementExistByAttributePatternAndText(HtmlElementType.SPAN, HtmlAttribute.CLASS, "fd-icon-tab-bar__tag", fileName);
+    }
+
+}
+
+
+
+
