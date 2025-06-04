43508325e6cbd2b63389c9cd9f4b40ee6b47727a
Nikol Georgieva <nicole.georgieva18@abv.bg>
2025-06-04 18:42:39 +0300

    add test for roles definition

--- tests/tests-framework/src/main/java/org/eclipse/dirigible/tests/framework/ide/IDEFactory.java
+++ tests/tests-framework/src/main/java/org/eclipse/dirigible/tests/framework/ide/IDEFactory.java
@@ -25,14 +25,17 @@ public class IDEFactory {
     private final WorkbenchFactory workbenchFactory;
     private final DatabasePerspectiveFactory databasePerspectiveFactory;
     private final GitPerspectiveFactory gitPerspectiveFactory;
+    private final SecurityPerspectiveFactory securityPerspectiveFactory;
 
     protected IDEFactory(BrowserFactory browserFactory, RestAssuredExecutor restAssuredExecutor, WorkbenchFactory workbenchFactory,
-            DatabasePerspectiveFactory databasePerspectiveFactory, GitPerspectiveFactory gitPerspectiveFactory) {
+            DatabasePerspectiveFactory databasePerspectiveFactory, GitPerspectiveFactory gitPerspectiveFactory,
+            SecurityPerspectiveFactory securityPerspectiveFactory) {
         this.browserFactory = browserFactory;
         this.restAssuredExecutor = restAssuredExecutor;
         this.workbenchFactory = workbenchFactory;
         this.databasePerspectiveFactory = databasePerspectiveFactory;
         this.gitPerspectiveFactory = gitPerspectiveFactory;
+        this.securityPerspectiveFactory = securityPerspectiveFactory;
     }
 
     public IDE create() {
@@ -46,11 +49,11 @@ public class IDEFactory {
 
     public IDE create(Browser browser, String username, String password) {
         return new IDE(browser, username, password, restAssuredExecutor, workbenchFactory, databasePerspectiveFactory,
-                gitPerspectiveFactory);
+                gitPerspectiveFactory, securityPerspectiveFactory);
     }
 
     public IDE create(String host, String username, String password) {
         return new IDE(browserFactory.createByHost(host), username, password, restAssuredExecutor, workbenchFactory,
-                databasePerspectiveFactory, gitPerspectiveFactory);
+                databasePerspectiveFactory, gitPerspectiveFactory, securityPerspectiveFactory);
     }
 }
