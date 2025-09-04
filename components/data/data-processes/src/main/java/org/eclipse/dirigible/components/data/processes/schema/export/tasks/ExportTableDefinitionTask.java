1e755228a7fa8a63a95c79541fe419689e19a8ae
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-09-04 10:27:30 +0300

    execute tasks for the registered tenant

--- components/data/data-processes/src/main/java/org/eclipse/dirigible/components/data/processes/schema/export/tasks/ExportTableDefinitionTask.java
+++ components/data/data-processes/src/main/java/org/eclipse/dirigible/components/data/processes/schema/export/tasks/ExportTableDefinitionTask.java
@@ -14,16 +14,12 @@ import org.eclipse.dirigible.components.data.processes.schema.export.ExportFiles
 import org.eclipse.dirigible.components.data.sources.manager.DataSourcesManager;
 import org.eclipse.dirigible.components.data.structures.domain.Table;
 import org.eclipse.dirigible.components.database.DirigibleDataSource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.sql.SQLException;
 
 @Component("ExportTableDefinitionTask") // used in the bpmn process
 class ExportTableDefinitionTask extends BaseExportTask {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(ExportTableDefinitionTask.class);
 
     private final DataSourcesManager datasourceManager;
     private final DataSourceMetadataLoader dataSourceMetadataLoader;
