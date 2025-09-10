9fc428366363cd3d40c547568909458137cd68d3
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-09-10 14:38:20 +0300

    Revert "change import schema determination logic"
    This reverts commit 79db3a3b71d46edd111750ac6988151c62752441.

--- components/data/data-csvim/src/main/java/org/eclipse/dirigible/components/data/csvim/processor/CsvimProcessor.java
+++ components/data/data-csvim/src/main/java/org/eclipse/dirigible/components/data/csvim/processor/CsvimProcessor.java
@@ -149,8 +149,8 @@ public class CsvimProcessor {
         try (Connection connection = dataSource.getConnection()) {
 
             String fileSchema = csvFile.getSchema();
-            String targetSchema = null != fileSchema ? fileSchema : (defaultDataSource ? connection.getSchema() : null);
-
+            String targetSchema =
+                    defaultDataSource ? ("PUBLIC".equalsIgnoreCase(fileSchema) ? connection.getSchema() : fileSchema) : fileSchema;
             if (null != targetSchema) {
                 connection.setSchema(targetSchema);
             }
