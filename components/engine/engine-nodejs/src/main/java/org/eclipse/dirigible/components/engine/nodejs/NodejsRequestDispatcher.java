80eb8b1864b6d7d9a5af1435e21e30059a6fe2c9
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-09-24 13:45:47 +0300

    wip: simple projects registry, dispatching logic

--- components/engine/engine-nodejs/src/main/java/org/eclipse/dirigible/components/engine/nodejs/NodejsRequestDispatcher.java
+++ components/engine/engine-nodejs/src/main/java/org/eclipse/dirigible/components/engine/nodejs/NodejsRequestDispatcher.java
@@ -7,21 +7,45 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.function.ServerRequest;
 
 import java.net.URI;
+import java.util.Optional;
 import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Component
 class NodejsRequestDispatcher implements Function<ServerRequest, ServerRequest> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(NodejsRequestDispatcher.class);
 
+    private static final Pattern PATH_PATTERN = Pattern.compile(NodejsProxyConfig.PATH_PATTERN_REGEX);
+
+    private final NodejsProjectsRegistry projectsRegistry;
+
+    NodejsRequestDispatcher(NodejsProjectsRegistry projectsRegistry) {
+        this.projectsRegistry = projectsRegistry;
+    }
+
     @Override
     public ServerRequest apply(ServerRequest request) {
-        String proxyURL = "http://localhost:3000";
-
         URI requestURI = request.uri();
-        LOGGER.info("--- Dispatching request with path [{}] to [{}]", requestURI, proxyURL);
+        LOGGER.debug("Dispatching request with URI {}", requestURI);
 
-        setRequestURL(request, proxyURL);
+        String path = requestURI.getPath();
+        Matcher matcher = PATH_PATTERN.matcher(path);
+        if (!matcher.matches()) {
+            throw new IllegalStateException(
+                    "Dispatcher is mapped on an invalid path. Path [" + path + "] doesn't match [" + PATH_PATTERN + "]");
+        }
+        String projectName = matcher.group(1);
+        Optional<NodejsProject> project = projectsRegistry.findByProjectName(projectName);
+        if (project.isEmpty()) {
+            throw new IllegalArgumentException(
+                    "Received request to path [" + path + "] which is for unregistered project with name [" + projectName + "]");
+        }
+
+        String projectURL = project.get()
+                                   .getMappedURL();
+        setRequestURL(request, projectURL);
 
         return request;
     }
