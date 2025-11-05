3e87a6af49a0a4857feeac3ecb73fd66bc0d793c
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-11-05 13:37:56 +0200

    Revert "test on package root files"
    This reverts commit 9b38c0672f8c58090184640ff399b70f751463ab.

--- components/api/api-modules-javascript/src/main/resources/META-INF/dirigible/modules/build-source.sh
+++ components/api/api-modules-javascript/src/main/resources/META-INF/dirigible/modules/build-source.sh
@@ -6,13 +6,13 @@ set -e
 rm -rf ./dist
 
 # build esm
-esbuild $(find . -iname '*.ts' -not -iname '*.d.ts') '--out-extension:.js=.mjs' --sourcemap=inline --outdir=. --format=esm --target=es2022
+esbuild $(find . -iname '*.ts' -not -iname '*.d.ts') '--out-extension:.js=.mjs' --sourcemap=inline --outdir=dist/esm --format=esm --target=es2022
 
 # build cjs
-esbuild $(find . -iname '*.ts' -not -iname '*.d.ts') --sourcemap=inline --outdir=. --format=cjs --target=es2022
+esbuild $(find . -iname '*.ts' -not -iname '*.d.ts') --sourcemap=inline --outdir=dist/cjs --format=cjs --target=es2022
 
 # build dts
 #tsc --emitDeclarationOnly --outDir dist/dts
 
 # build types
-tsc --emitDeclarationOnly --outDir dist
+tsc --emitDeclarationOnly --outDir dist/types
