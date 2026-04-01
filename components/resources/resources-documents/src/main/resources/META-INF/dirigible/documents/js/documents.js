a8c23feacaf55f75eaa8fbc60e731e11b47c367e
StanGenchev <82379274+StanZGenchev@users.noreply.github.com>
2026-04-01 12:01:31 +0300

    Fixed #5806 (#5807)
    Signed-off-by: StanZGenchev <stan.genchev@codbex.com>

--- components/resources/resources-documents/src/main/resources/META-INF/dirigible/documents/js/documents.js
+++ components/resources/resources-documents/src/main/resources/META-INF/dirigible/documents/js/documents.js
@@ -154,6 +154,7 @@ documents.controller('DocumentsController', ($scope, $http, $timeout, $element, 
     $scope.hasForward = () => $scope.history.hasForward();
     $scope.goBack = () => $scope.history.goBack(path => loadFolder(path));
     $scope.goForward = () => $scope.history.goForward(path => loadFolder(path));
+    $scope.refresh = refreshFolder;
 
     function getAllSelectedFilePaths() {
         let paths = [];
